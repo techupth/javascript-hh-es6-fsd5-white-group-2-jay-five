5178440c23539f74e1d1f489ddb024ccd64dfa8b
Jaturapat Dusadee <mr.jaturapat.d@gmail.com>
2023-12-13 12:47:19 +0700

    Finish exercise Hackhour

--- ex-5.js
+++ ex-5.js
@@ -1,5 +1,10 @@
-function sum() {
+function sum(...number) {
   // Start coding here !
+  const totalSum = number.reduce((accumulator, currentValue) => {
+    accumulator += currentValue;
+    return accumulator;
+  }, 0);
+  return totalSum;
 }
 
 let result1 = sum(1, 2, 3, 4, 5, 6, 7, 8);
--- ex-6.js
+++ ex-6.js
@@ -1,4 +1,10 @@
 let height = undefined;
 let result;
 
+function checkHeight(param) {
+  return (result = param ?? "Height is not defined");
+}
+
+checkHeight(height);
+
 console.log(result); //  Result ควรจะได้ออกมาเป็น “Height is not defined”
